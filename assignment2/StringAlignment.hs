515c6da106fe7326b61007f470960607e5179806
Fredrik <nyberg_11@hotmail.com>
2017-05-02 12:42:36 +0200

    added code skeleton

--- assignment2/StringAlignment.hs
+++ assignment2/StringAlignment.hs
@@ -1,1 +1,36 @@
 -- String Alignment assignment
+
+scoreMatch = 1
+scoreMismatch = -1
+scoreSpace = -2
+
+type AlignmentType = (String,String)
+
+--optAlignments :: String -> String -> [AlignmentType]
+--optAlignments string1 string2
+
+
+
+similarityScore :: String -> String -> Int
+similarityScore (x:xs) (y:ys) = max match $max xSpacematch ySpacematch
+    where match = similarityScore xs ys + (score x y)
+          xSpacematch = similarityScore xs (y:ys) + (score x '-')
+          ySpacematch = similarityScore (x:xs) ys + (score '-' y)
+
+
+
+score :: Char -> Char -> Int  
+score x '-' = scoreSpace
+score '-' y = scoreSpace
+score x y
+    | x == y = scoreMatch
+    | otherwise = scoreMismatch
+
+
+attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])] 
+attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
+
+--maximaBy :: Ord b => (a -> b) -> [a] -> [a] 
+--maximaBy valueFcn xs
+
+--outputOptAlignments string1 string2
