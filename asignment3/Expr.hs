5bb58d413e38a626806575b00476ffcbf6e8e3d9
Martin Sundeqvist <sys12msu@alfa-6.student.lth.se>
2017-05-12 15:04:54 +0200

    Finished value in Expr, tested successfully with TestExpr.hs

--- asignment3/Expr.hs
+++ asignment3/Expr.hs
@@ -71,7 +71,17 @@ shw prec (Mul t u) = parens (prec>6) (shw 6 t ++ "*" ++ shw 6 u)
 shw prec (Div t u) = parens (prec>6) (shw 6 t ++ "/" ++ shw 7 u)
 
 value :: Expr -> Dictionary.T String Integer -> Integer
-value (Num n) _ = error "value not implemented"
+value (Num n) _ = n
+value (Var v) dict = 
+    case (Dictionary.lookup v dict) of
+        Nothing -> error $ "undefined variable "++v
+        Just b -> b
+value (Add t u) dict = value t dict + value u dict
+value (Sub t u) dict = value t dict - value u dict
+value (Mul t u) dict = value t dict * value u dict
+value (Div t u) dict 
+        | value u dict == 0 = error "division by 0"
+        | otherwise = value t dict `div`  value u dict
 
 instance Parse Expr where
     parse = expr
